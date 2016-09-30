9bfa39df05265ed9bafefbeb9b5e302f020798fe
hsenidVidushka <vidushka@hsenidlanka.com>
2016-09-30 01:03:29 -0500

    question 1 part a complete code

---
+++ question1/src/main/java/Question1.java
@@ -0,0 +1,96 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by Vidushka on 9/29/16.
+ */
+public class Question1 {
+    public static void main(String[] args) {
+        sortNumber();
+    }
+
+    /**
+     * Method get input numbers from user and sort them
+     */
+    public static void sortNumber() {
+        for (; ; ) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter numbers seperated by a space. ");
+            String numberToSort = input.nextLine();
+            String[] numberArray = numberToSort.split("\\s+");
+            /**
+             * convert the string array to int array
+             */
+            int[] arr = new int[numberArray.length];
+            for (int i = 0; i < numberArray.length; i++) {
+                arr[i] = Integer.parseInt(numberArray[i]);
+            }
+
+            Scanner order = new Scanner(System.in);
+            System.out.println("How do you want to show sorted numbers Assending(a)/ Dissending(d)?");
+            String outputOrder = order.nextLine();
+            if (outputOrder.equals("a")) {
+                Scanner output = new Scanner(System.in);
+                System.out.println("How do you want to show sorted numbers horizontally(h)/ vertically(v)?");
+                String outputFormat = output.nextLine();
+
+                if (outputFormat.equals("h")) {
+                    Arrays.sort(arr);
+                    for (int j = 0; j < arr.length; j++) {
+                        System.out.println(arr[j]);
+                    }
+                } else if (outputFormat.equals("v")) {
+                    Arrays.sort(arr);
+                    for (int k = 0; k < arr.length; k++) {
+                        if (k != arr.length - 1)
+                            System.out.print(arr[k] + ", ");
+                        else
+                            System.out.print(arr[k]);
+                    }
+                } else {
+                    System.out.println("Please enter v or h.");
+                }
+                Scanner terminate = new Scanner(System.in);
+                System.out.println("\nType x to exit or press any key to continue");
+                String endProcess = terminate.nextLine();
+                if (endProcess.equals("x"))
+                    break;
+                else
+                    continue;
+            }
+            else if (outputOrder.equals("d")){
+                Scanner output = new Scanner(System.in);
+                System.out.println("How do you want to show sorted numbers horizontally(h)/ vertically(v)?");
+                String outputFormat = output.nextLine();
+
+                if (outputFormat.equals("h")) {
+                    Arrays.sort(arr);
+                    int q=arr.length-1;
+                    for (int j=q; j>=0; j--) {
+                        System.out.println(arr[j]);
+                    }
+                } else if (outputFormat.equals("v")) {
+                    Arrays.sort(arr);
+                    for (int k=arr.length-1; k>=0; k--) {
+                        if (k != 0)
+                            System.out.print(arr[k] + ", ");
+                        else
+                           System.out.print(arr[k]);
+                    }
+                } else {
+                    System.out.println("Please enter v or h.");
+                }
+                Scanner terminate = new Scanner(System.in);
+                System.out.println("\nType x to exit or press any key to continue");
+                String endProcess = terminate.nextLine();
+                if (endProcess.equals("x"))
+                    break;
+                else
+                    continue;
+            }
+            else
+                System.out.println("Type 'a' or 'd'.");
+        }
+    }
+}
